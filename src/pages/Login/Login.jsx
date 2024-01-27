fdff7033060879852c320fa26955b938e573246f
unknown <mahmudul.cse.eng@gmail.com>
2024-01-27 23:55:45 +0600

    some props updated

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
             // get access token
             axios.post('http://localhost:5000/jwt', user, {withCredentials: true})
             .then(res => {
-                console.log(res.data)
+                
                 if(res.data.success){
                     navigate(location?.state?  location.state : '/' )
                 }
